6ab54d4335c4ef16b026aa651b3313aea0792100
agr17 <aregueiro1700@gmail.com>
2022-06-09 23:05:15 +0200

    Basic control

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from constants import *
 import pygame
 
 if __name__ == '__main__':
+
+    # initialize 
+    pygame.init()
     pygame.display.set_caption("Pygame Wordle")
     
     # Screen
@@ -14,6 +17,17 @@ if __name__ == '__main__':
 
     while running:
         for event in pygame.event.get():
+
+            # Get the key if is a letter
+            if event.type == pygame.KEYDOWN and event.key >= 97 and event.key <= 122:
+                print(pygame.key.name(event.key))
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
+                print("Backspace")
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                print("Enter")
+
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
