e9cb7871dc137f8febad49662a3d4217bacbf260
agr17 <aregueiro1700@gmail.com>
2022-06-10 10:28:12 +0200

    Version 1.0

--- src/wordle.py
+++ src/wordle.py
@@ -1,6 +1,8 @@
 from box import Box
 import pygame
 
+from constants import FAIL, SEMI_SUCCESS, SUCCESS
+
 class Wordle():
     def __init__(self, screen, word_length, target_word):
         self.screen = screen
@@ -14,20 +16,33 @@ class Wordle():
 
     def write_letter(self, letter):
         if self.actual_letter < self.word_length:
-            print(f'Writing letter {letter} in position {self.actual_line}:{self.actual_letter}')
+            # print(f'Writing letter {letter} in position {self.actual_line}:{self.actual_letter}')
+            self.game_grid[self.actual_line][self.actual_letter].set_text(letter, self.screen)
             self.actual_letter += 1
         else:
             print(f'ERROR: Line {self.actual_line} is full!')        
 
     def delete_letter(self):
         if self.actual_letter > 0:
-            print(f'Deleting letter in position {self.actual_line}:{self.actual_letter}')
             self.actual_letter -= 1
+            # print(f'Deleting letter in position {self.actual_line}:{self.actual_letter}')
+            self.game_grid[self.actual_line][self.actual_letter].delete_text(self.screen)
         else:
             print(f'ERROR: Line {self.actual_line} is empty!')
 
     def enter_word(self):
-        print('word ready!')
+        print(f"Actual letter: {self.actual_letter} and word length: {self.word_length}")
+        if self.actual_letter == self.word_length:
+            win = self._check_word()
+            if win:
+                print('You win!')
+            elif self.actual_line == self.word_length - 1:
+                print('You lose!')
+            else:
+                self.actual_line += 1
+                self.actual_letter = 0
+        else:
+            print('You must finish the line!')
 
     def create_level(self):
         separation = 20
@@ -45,3 +60,21 @@ class Wordle():
                 grid_line.append(aux_box)
 
             self.game_grid.append(grid_line)
+
+    def _check_word(self):
+        win = True
+        for i in range(self.word_length):
+            letter_aux = self.game_grid[self.actual_line][i].get_text()
+            
+            if letter_aux == self.target_word[i]:
+                state = SUCCESS
+            elif letter_aux in self.target_word:
+                state = SEMI_SUCCESS
+                win = False
+            else:
+                state = FAIL
+                win = False
+
+            self.game_grid[self.actual_line][i].set_state(state, self.screen)
+
+        return win
